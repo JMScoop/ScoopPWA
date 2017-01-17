336c7827e8ab6d880a366699c18b5de1c37916c9
brown2mm <brown2mm@dukes.jmu.edu>
2017-01-17 17:55:58 -0500

    Login and Logout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpModule, Http, RequestOptions } from '@angular/http';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { provideAuth, AuthHttp, AuthConfig } from 'angular2-jwt';
 
 import { AppComponent } from './app.component';
+
+import { Auth } from './services/auth.service';
+
+//import { AUTH_PROVIDERS }      from 'angular2-jwt';
+
+//import { routing, appRoutingProviders } from './app.routes';
+
+//import { HomeComponent }       from './home.component';
+
+export function authHttpServiceFactory(http: Http, options: RequestOptions) {
+  return new AuthHttp( new AuthConfig({}), http, options);
+}
 
 @NgModule({
   declarations: [
@@ -16,7 +29,14 @@ import { AppComponent } from './app.component';
     HttpModule,
     NgbModule.forRoot()
   ],
-  providers: [],
+  providers: [    
+    {
+      provide: AuthHttp,
+      useFactory: authHttpServiceFactory,
+      deps: [ Http, RequestOptions ]
+    }, 
+    Auth
+    /*AUTH_PROVIDERS*/],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
