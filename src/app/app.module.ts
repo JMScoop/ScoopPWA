a693c3355cebff7f7ece03435aa44d8d88e24510
unknown <ellisobl@dukes.jmu.edu>
2017-10-16 12:33:26 -0400

    added mycarpools page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
 import { IneedaridePage } from '../pages/ineedaride/ineedaride';
 import { ProfilePage } from '../pages/profile/profile';
+import { MycarpoolsPage } from '../pages/mycarpools/mycarpools';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -17,7 +18,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     HomePage,
     ListPage,
     IneedaridePage,
-    ProfilePage
+    ProfilePage,
+    MycarpoolsPage
   ],
   imports: [
     BrowserModule,
@@ -29,7 +31,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     HomePage,
     ListPage,
     IneedaridePage,
-    ProfilePage
+    ProfilePage,
+    MycarpoolsPage
   ],
   providers: [
     StatusBar,
