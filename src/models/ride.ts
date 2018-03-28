8b9708c8f13a138b4c13b0026cfab1bac5d6ef44
Morgan Benton <registered@morphatic.com>
2018-03-28 18:39:30 -0400

    post-a-ride works

--- src/models/ride.ts
+++ src/models/ride.ts
@@ -2,13 +2,39 @@ import {Person} from './person';
 
 export class Ride {
 	constructor (
+		public driver: Person,
 		public departs: Date,
 		public origin: string = "",
 		public destination: string = "",
-		public notes: string = "",
 		public seats_open: number = 3,
-		public passengers: Person[],
-		public driver: Person
+		public notes: string = "",
+		public passengers: Person[] = [],
 	){}
-	
+
 }
+
+export const locations = [
+	"Sunchase Apartments",
+	"Charleston Townes",
+	"Copper Beech",
+	"The Hills: Stonegate",
+	"The Harrison",
+	"Forest Hills Manor LLC",
+	"Forest Hills LLC",
+	"Urban Exchange",
+	"Downtown",
+	"Pheasant Run",
+	"Aspen Heights",
+	"Foxhill Townes",
+	"865",
+	"North 38",
+	"Hunters Ridge",
+	"The Mill",
+	"Campus View Apartments",
+	"The Retreat",
+	"Squire Hill",
+	"Campus: The Quad",
+	"Campus: Lakeside",
+	"Campus: East Campus",
+	"Campus: Memorial Hall"
+];
--- src/pages/post-a-ride/post-a-ride.ts
+++ src/pages/post-a-ride/post-a-ride.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { Validators, FormBuilder, FormGroup, ValidatorFn, AbstractControl } from '@angular/forms';
 import * as moment from 'moment';
 import { Car } from '../../models/car'
-import { Ride } from '../../models/ride'
+import { Ride, locations } from '../../models/ride'
 import { Person } from '../../models/person'
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
@@ -12,6 +12,10 @@ import { MyCarpoolsPage } from '../my-carpools/my-carpools';
 interface RidePost {
   depart_date: string;
   depart_time: string;
+  leaving_from?: string;
+  going_to?: string;
+  seats_available?: number;
+  notes?: string;
 }
 
 @IonicPage()
@@ -21,22 +25,19 @@ interface RidePost {
 })
 export class PostARidePage {
 
-// export class Ride {
-//   constructor (
-//     public departs: Date,
-//     public origin: string = "",
-//     public destination: string = "",
-//     public notes: string = "",
-//     public seats_open: number = 3,
-//     public passengers: Person[],
-//     public driver: Person
-//   ){}
-
   private today: moment.Moment = moment();
+  private locs = locations;
+  private car: Car = new Car(4, "Light Blue Honda CRV");
+  private user: Person = new Person("Brenna", "Ellison", this.car);
+
+  // show or hide error message flags
+  private showLeavingFromError: boolean = false;
+
 
   private ride: RidePost = {
     depart_date: this.today.format('Y-MM-DD'),
-    depart_time: this.today.format('h:mm a')
+    depart_time: this.today.format('h:mm a'),
+    seats_available: 3
   };
 
   constructor(
@@ -45,56 +46,62 @@ export class PostARidePage {
     private fb: FormBuilder,
     private rideProvider: RideProvider
   ) {
-    // this.ride = fb.group({
-    //   departs: [ new Date(), [ Validators.required, this.departureDateValidator ] ]
-    // });
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad PostARidePage');
+    // get the number of seats in the car
+    // of the currently logged-in user and
+    // use it to set this.ride.seats_available
+    //
+    // this.ride.seats_available = this.user.car.seats;
   }
-
-  // departureDateValidator(): ValidatorFn {
-  //   return (control: AbstractControl): { [key: string]: any } => {
-  //     // set comparison value as last night at midnight
-  //     const midnightLastNight = new Date();
-  //     midnightLastNight.setHours(0, 0, 0, 0);
-
-  //     // create a useful error object
-  //     const departureDateError = {
-  //       invalidDepartureDate: {
-  //         value: control.value,
-  //         message: "Departure date must be in the future"
-  //       }
-  //     };
-
-  //     // check to see that 'departs' is AFTER last night at midnight
-  //     return control.value < midnightLastNight ? departureDateError : null;
-  //   };
-  // }
 
   postRide() {
     console.log(this.ride);
     // process all of our values, e.g. combine depart_date + depart_time
-    const departs = new Date(this.ride.depart_date + this.ride.depart_time);
+
+    // check our datetime values
+    const now = new Date();
+    const departs = new Date(`${this.ride.depart_date} ${this.ride.depart_time}`);
+    // is the departure in the future?
+    if (departs < now) {
+      // uh-oh, the departure time is in the past!!!
+    }
+
+    // handle if either location is not set
+    if (!this.ride.leaving_from || "" === this.ride.leaving_from) {
+      // uh-oh!
+      this.showLeavingFromError = true;
+      console.log(this.showLeavingFromError);
+      return;
+    }
+    if ("" === this.ride.going_to) {
+      // uh-oh!
+    }
+
+    // create a new ride object
     let newRide = new Ride(
+      this.user,
       departs,
-      "Festival",
-      "Stonegate",
-      "",
-      3,
-      [],
-      new Person("Morgan", "Benton", new Car(3, ""))
+      this.ride.leaving_from,
+      this.ride.going_to,
+      this.ride.seats_available,
+      this.ride.notes,
+      []
     );
-    this.rideProvider.postRide(newRide).subscribe(
+    this.rideProvider.postRide(newRide).then(
       (res: any) => {
         // result from calling postRide comes back and
-        if (Error) {
-          // handle the error
-        }
+        // if (Error) {
+        //   // handle the error
+        // }
         // otherwise
         // navigate to the next page... my-carpools?
-        this.navCtrl.push(MyCarpoolsPage);
+        // this.navCtrl.push(MyCarpoolsPage);
+        console.log(res);
+      },
+      (err: any) => {
+        console.log("ERROR: ", err);
       }
     );
   }
